90a4e40a46002e653b930dd56d18beb6a9b8e51b
Tunde <ogunjimi.tunde@turnkeyafrica.com>
2022-04-09 07:16:22 +0100

    Update images & Dockerfile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TrainingComponent } from './training/training.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectsComponent,
+    TrainingComponent
   ],
   imports: [
     BrowserModule,
