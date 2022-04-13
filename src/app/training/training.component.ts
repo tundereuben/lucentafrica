d435b48a7193f61ad32f090d990226682532fc11
Tunde Ogunjimi <tundereuben@yahoo.com>
2022-04-13 15:42:09 +0100

    Updated contact email address

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -18,30 +18,35 @@ export class TrainingComponent implements OnInit {
     this.courses = [
       {
         title: 'Full Stack Web Development',
+        description: 'Learn frontend, backend and everything in-between to build and deliver scalable web applications',
         content: ['HTML', 'CSS', 'JavaScript', 'Node', 'NPM', 'Express', 'MongoDB', 'Mongoose'],
         imageUrl: 'full-stack.webp',
         courseUrl: 'full-stack'
       },
       {
         title: 'HTML & CSS',
+        description: 'Use HTML5 and CSS3 to build website content and add beautiful styling and decoration',
         content: ['HTML', 'CSS'],
         imageUrl: 'html-css.jpg',
         courseUrl: 'html-css'
       },
       {
         title: 'Programming in JavaScript',
+        description: 'Start with the fundamentals and progresses to advanced programming concepts.',
         content: ['JavaScript'],
         imageUrl: 'javascript.jpg',
         courseUrl: 'javascript'
       },
       {
         title: 'Bootstrap',
+        description: 'Create a mobile-first responsive website that adapts to any screen size',
         content: ['BootStrap'],
         imageUrl: 'bootstrap.jpg',
         courseUrl: 'bootstrap'
       },
       {
         title: 'WordPress Web Design',
+        description: 'Learn how to develop and manage websites without writing code',
         content: ['WordPress'],
         imageUrl: 'wordpress.jpg',
         courseUrl: 'wordpress'
@@ -54,6 +59,7 @@ export class TrainingComponent implements OnInit {
       },*/
       {
         title: 'Office Workspace Basics',
+        description: 'Learn computer fundamentals, word processing, spreadsheets and presentations',
         content: ['Workspace Basics', 'Microsoft Word', 'Microsoft Excel', 'Microsoft PowerPoint'],
         imageUrl: 'office.webp',
         courseUrl: 'office-workspace'
@@ -69,6 +75,7 @@ export class TrainingComponent implements OnInit {
 
 export interface Course {
   title: string;
+  description?: string;
   content: string[];
   duration?: string;
   imageUrl?: string;
